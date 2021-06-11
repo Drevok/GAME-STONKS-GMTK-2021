421cb2b7eb69b384b1b952b52be84842cf60116e
Drevok <theoartous@hotmail.fr>
2021-06-12 01:39:11 +0200

    Le magnet reste 3 secondes actif puis se désactive

--- Assets/MagnetScript.cs
+++ Assets/MagnetScript.cs
@@ -6,30 +6,35 @@ using UnityEngine;
 public class MagnetScript : MonoBehaviour
 {
     public GameObject LinePrefab;
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private float timeMagnet = 3f;
 
     private void Start()
     {
-
+        timeMagnet = 10f;
     }
 
     public void ActivateMagnet()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 4f);
-        foreach (Collider c in colliders)
-        {
-            if (c.GetComponent<Rigidbody>())
-            {
-                //Instantiate(LinePrefab) ;
-                //LinePrefab.GetComponent<LineScipt>().GetNewPositions(transform.position, c.transform.position);
-                Vector3 distanceFromMagnetable = (c.transform.position - transform.position) * 100;
-                c.GetComponent<Rigidbody>().AddForce(-distanceFromMagnetable);
+        timeMagnet = 0;
+    }
 
+    private void Update()
+    {
+        if (timeMagnet <= 3f)
+        {
+            timeMagnet = timeMagnet + Time.deltaTime;
+            
+            Collider[] colliders = Physics.OverlapSphere(transform.position, 4f);
+            foreach (Collider c in colliders)
+            {
+                if (c.GetComponent<Rigidbody>())
+                {
+                    //Instantiate(LinePrefab) ;
+                    //LinePrefab.GetComponent<LineScipt>().GetNewPositions(transform.position, c.transform.position);
+                    Vector3 distanceFromMagnetable = (c.transform.position - transform.position) * 100;
+                    c.GetComponent<Rigidbody>().AddForce(-distanceFromMagnetable);
+
+                }
             }
         }
     }
