b275d5d2a406b9c0a04d815cd69c787c9e7e8c28
Drevok <theoartous@hotmail.fr>
2021-06-13 14:04:31 +0200

    Ajouté l'arrivée (pourrave)

---
+++ Assets/GoalScript.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        Win();
+    }
+
+    void Win()
+    {
+        SceneManager.LoadScene("WinScene");
+    }
+}
